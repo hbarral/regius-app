d057407cd5493b1392372f399ea3d0a217dc7e7b
Héctor Barral <hbarral@mail.ru>
2023-11-04 16:09:20 -0300

    UPDATE vendor

--- vendor/gitlab.com/hbarral/regius/helpers.go
+++ vendor/gitlab.com/hbarral/regius/helpers.go
@@ -1,6 +1,25 @@
 package regius
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_+"
+)
+
+func (r *Regius) RandomString(n int) string {
+	a, b := make([]rune, n), []rune(randomString)
+
+	for i := range a {
+		p, _ := rand.Prime(rand.Reader, len(b))
+		x, y := p.Uint64(), uint64(len(b))
+		a[i] = b[x%y]
+	}
+
+	return string(a)
+}
 
 func (c *Regius) CreateDirIfNotExist(path string) error {
 	const mode = 0755
