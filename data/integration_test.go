905973353271fb7b3c60eaf525f382a71bbd12f3
Héctor Barral <hbarral@mail.ru>
2023-11-06 14:54:44 -0300

    START with integration tests

---
+++ data/integration_test.go
@@ -0,0 +1,146 @@
+// go:build integration
+// run tests with this command: go test . --tags integration --count=1
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "secret"
+	dbName   = "regius"
+	port     = "5435"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timzone=UTC connect_timeout=5"
+)
+
+var dummyUser = User{
+	FirstName: "Some",
+	LastName:  "Guy",
+	Email:     "me@someguy.com",
+	Active:    1,
+	Password:  "password",
+}
+
+var models Models
+var testDB *sql.DB
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+
+func TestMain(m *testing.M) {
+	os.Setenv("DATABASE_TYPE", "postgres")
+
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.4",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("Could not start resources: %s", err)
+	}
+
+	if err := pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("Could not connect to docker: %s", err)
+	}
+
+	err = createTables(testDB)
+	if err != nil {
+		log.Fatalf("Error creating tables: %s", err)
+	}
+
+	models = New(testDB)
+
+	code := m.Run()
+
+	os.Exit(code)
+
+}
+
+func createTables(db *sql.DB) error {
+	stmt := `
+drop table if exists users cascade;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(255) NOT NULL,
+    last_name character varying(255) NOT NULL,
+    user_active integer NOT NULL DEFAULT 0,
+    email character varying(255) NOT NULL UNIQUE,
+    password character varying(60) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+drop table if exists remember_tokens;
+
+CREATE TABLE remember_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    remember_token character varying(100) NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now()
+);
+
+drop table if exists tokens;
+
+CREATE TABLE tokens (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    first_name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL UNIQUE,
+    token_hash bytea NOT NULL,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    updated_at timestamp without time zone NOT NULL DEFAULT now(),
+    expiry timestamp without time zone NOT NULL
+)
+  `
+	_, err := db.Exec(stmt)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestUser_Table(t *testing.T) {
+	s := models.Users.Table()
+	if s != "users" {
+		t.Error("Wrong table name returned: ", s)
+	}
+}
