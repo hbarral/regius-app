d72711d305f79d50503ac2aac2b8b4e260aa4a2f
Héctor Barral <hbarral@mail.ru>
2024-01-24 15:37:27 -0300

    IMPROVE database type checking

--- data/models.go
+++ data/models.go
@@ -1,19 +1,19 @@
 package data
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
 
 	db2 "github.com/upper/db/v4"
-
 	"github.com/upper/db/v4/adapter/mysql"
 	"github.com/upper/db/v4/adapter/postgresql"
-
-	"database/sql"
-	"os"
 )
 
-var db *sql.DB
-var upper db2.Session
+var (
+	db    *sql.DB
+	upper db2.Session
+)
 
 type Models struct {
 	// any models inserted here (and in the New function)
@@ -25,10 +25,13 @@ type Models struct {
 func New(databasePool *sql.DB) Models {
 	db = databasePool
 
-	if os.Getenv("DATABASE_TYPE") == "mysql" || os.Getenv("DATABASE_TYPE") == "mariadb" {
+	switch os.Getenv("DATABASE_TYPE") {
+	case "mysql", "mariadb":
 		upper, _ = mysql.New(databasePool)
-	} else {
+	case "postgres", "postgresql":
 		upper, _ = postgresql.New(databasePool)
+	default:
+		// do nothing
 	}
 
 	return Models{
