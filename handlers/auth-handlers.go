ccd8998e5c173bf1b25b054a059291459ca2909f
Héctor Barral <hbarral@mail.ru>
2024-02-20 23:37:18 -0300

    ADD remember me functionality to SignIn and SignOut methods

--- handlers/auth-handlers.go
+++ handlers/auth-handlers.go
@@ -1,7 +1,12 @@
 package handlers
 
 import (
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
 	"net/http"
+	"regius-app/data"
+	"time"
 )
 
 func (h *Handlers) UserSignIn(w http.ResponseWriter, r *http.Request) {
@@ -38,14 +43,70 @@ func (h *Handlers) PostUserSignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Form.Get("remember") == "remember" {
+		randomString := h.randomString(12)
+		hasher := sha256.New()
+
+		_, err := hasher.Write([]byte(randomString))
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+		rm := data.RememberToken{}
+		err = rm.InsertToken(user.ID, sha)
+		if err != nil {
+			h.App.ErrorStatus(w, http.StatusBadRequest)
+			return
+		}
+
+		expire := time.Now().Add(365 * 24 * 60 * 60 * time.Second)
+		cookie := http.Cookie{
+			Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+			Value:    fmt.Sprintf("%d|%s", user.ID, sha),
+			Path:     "/",
+			Expires:  expire,
+			HttpOnly: true,
+			Domain:   h.App.Session.Cookie.Domain,
+			MaxAge:   315350000,
+			Secure:   h.App.Session.Cookie.Secure,
+			SameSite: http.SameSiteStrictMode,
+		}
+
+		http.SetCookie(w, &cookie)
+		h.App.Session.Put(r.Context(), "rememberToken", sha)
+	}
+
 	h.App.Session.Put(r.Context(), "userID", user.ID)
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-
 }
 
 func (h *Handlers) SignOut(w http.ResponseWriter, r *http.Request) {
+	if h.App.Session.Exists(r.Context(), "remember_token") {
+		rt := data.RememberToken{}
+		_ = rt.Delete(h.App.Session.GetString(r.Context(), "remember_token"))
+	}
+
+	newCookie := http.Cookie{
+		Name:     fmt.Sprintf("_%s_remember", h.App.AppName),
+		Value:    "",
+		Path:     "/",
+		Expires:  time.Now().Add(-100 * time.Hour),
+		HttpOnly: true,
+		Domain:   h.App.Session.Cookie.Domain,
+		MaxAge:   -1,
+		Secure:   h.App.Session.Cookie.Secure,
+		SameSite: http.SameSiteStrictMode,
+	}
+
+	http.SetCookie(w, &newCookie)
+
 	h.App.Session.RenewToken(r.Context())
 	h.App.Session.Remove(r.Context(), "userID")
+	h.App.Session.Remove(r.Context(), "remember_token")
+	h.App.Session.Destroy(r.Context())
+	h.App.Session.RenewToken(r.Context())
 	http.Redirect(w, r, "/users/signin", http.StatusSeeOther)
 }
