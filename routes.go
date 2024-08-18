eb589a844dc277bdd1931ae7db91ab1df23c638e
Héctor Barral <hbarral@mail.ru>
2024-08-18 16:16:04 -0400

    ADD /auth/provider routes

--- routes.go
+++ routes.go
@@ -15,6 +15,8 @@ func (a *application) routes() *chi.Mux {
 	// routes
 	a.get("/users/signin", a.Handlers.UserSignIn)
 	a.post("/users/signin", a.Handlers.PostUserSignIn)
+	a.get("/auth/{provider}")
+	a.get("/auth/{provider}/callback", a.Handlers.SocialCallback)
 	a.get("/", a.Handlers.Home)
 	a.get("/upload", a.Handlers.RegiusUpload)
 	a.post("/upload", a.Handlers.PostRegiusUpload)
