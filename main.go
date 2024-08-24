0419d9ee9ae6fc1da1eb51b3a1732a9a83ba68f0
Héctor Barral <hbarral@mail.ru>
2024-08-24 12:06:17 -0400

    ADD graceful shutdown method

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+
+	"gitlab.com/hbarral/regius"
+
 	"regius-app/data"
 	"regius-app/handlers"
 	"regius-app/middleware"
-
-	"gitlab.com/hbarral/regius"
 )
 
 type application struct {
@@ -13,9 +18,28 @@ type application struct {
 	Handlers   *handlers.Handlers
 	Models     data.Models
 	Middleware *middleware.Middleware
+	wg         sync.WaitGroup
 }
 
 func main() {
 	r := initApplication()
-	r.App.ListenAndServe()
+	go r.listenForShutdown()
+	err := r.App.ListenAndServe()
+	r.App.ErrorLog.Println(err)
+}
+
+func (a *application) shutdown() {
+	// put any clean up tasks here
+
+	// block until the WaitGroup is empty
+	a.wg.Wait()
+}
+
+func (a *application) listenForShutdown() {
+	osSignals := make(chan os.Signal, 1)
+	signal.Notify(osSignals, syscall.SIGINT, syscall.SIGTERM)
+	s := <-osSignals
+	a.App.InfoLog.Println("Received signal:", s)
+	a.shutdown()
+	os.Exit(0)
 }
