d11bb5b25d97530467b46526aaebb79702991fbc
Héctor Barral <hbarral@mail.ru>
2024-08-26 21:09:18 -0400

    ADD test with session for Home Page

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
@@ -18,5 +19,24 @@ func TestHome(t *testing.T) {
 
 	if response.StatusCode != 200 {
 		t.Errorf("expected status code 200, but got %d", response.StatusCode)
+	}
+}
+
+func TestHomeWithSession(t *testing.T) {
+	req, _ := http.NewRequest("GET", "/", nil)
+	ctx := getCtx(req)
+	req = req.WithContext(ctx)
+
+	responseRecorder := httptest.NewRecorder()
+	reg.Session.Put(ctx, "test_key", "test_value")
+	h := http.HandlerFunc(testHandlers.Home)
+	h.ServeHTTP(responseRecorder, req)
+
+	if responseRecorder.Code != 200 {
+		t.Errorf("expected status code 200, but got %d", responseRecorder.Code)
+	}
+
+	if reg.Session.GetString(ctx, "test_key") != "test_value" {
+		t.Errorf("session values do not match, want %v, got %v", "test_value", reg.Session.GetString(ctx, "test_key"))
 	}
 }
